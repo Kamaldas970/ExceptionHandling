14ced580b169437b403149d29a6f5bf980322069
Kamaldas970 <baleraokamal97050sravan@gmail.com>
2026-03-11 10:18:14 +0530

    Add files via upload

---
+++ stackOverflow.java
@@ -0,0 +1,14 @@
+class course{
+    public void teluskoC()
+    {
+        teluskoC();
+    }
+}
+public class stackOverflow
+{
+    public static void main(String args[]){
+        course t=new course();
+        System.out.println("Hello World");
+        t.teluskoC();
+    }
+}
