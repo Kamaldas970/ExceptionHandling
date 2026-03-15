520346e8e8264bc696f17d4db8efd87d95ef7844
Kamaldas970 <baleraokamal97050sravan@gmail.com>
2026-03-15 10:36:58 +0530

    Add files via upload

---
+++ NullPointer.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class throwKey{
+
+   static class alpha{
+        void divide(){
+          
+            System.out.println("The calC is Started");
+            Scanner sc=new Scanner(System.in);
+              System.out.println("Kindly enter the numarator");
+            int n=sc.nextInt();
+            System.out.println("Kindly enter the denominator");
+            int d=sc.nextInt();
+
+            int res=n/d;
+
+            System.out.println("The result is : " + res);
+            
+          
+
+
+        }
+    }
+    public static void main(String args[]){
+        throwKey.alpha n=new throwKey.alpha();
+  try{
+        n.divide();
+  }
+          catch(NullPointerException e){
+                System.out.println("Exception handled in divide method");
+                throw e;
+            }
+            finally{
+                System.out.println("Aft throw executed");
+            }
+
+    }
+}
